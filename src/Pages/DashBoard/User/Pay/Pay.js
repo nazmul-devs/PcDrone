ad7821ada441d71d21106d5787f263e4bc45dc9c
Nazmul Hosen <nazmulhosenm668@gmail.com>
2022-04-17 16:05:57 +0600

    review update

--- src/Pages/DashBoard/User/Pay/Pay.js
+++ src/Pages/DashBoard/User/Pay/Pay.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 
 import { loadStripe } from "@stripe/stripe-js";
-import { useParams } from "react-router";
 import CheckoutForm from "./CheckoutForm";
 
 const stripePromise = loadStripe(
